aa05605c0ae298b639289c3e44959d0985293124
Jeff Cela <cela.j@labrador-company.com>
2021-08-01 18:46:13 -0400

    finished the js script with the function for the color changing based on current time, added in comments for what things do, updated the README file

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # 05-Third-Party-APIs
+
+Use the provided framework to add in the script to match the gif mock up that was provided. The goal was to have a task track that when you added a task to a block of time you could save it. This saved information would stay with the user as it is stored in local storage. To bring a sense of organization and keep the user on track, the tool uses time to determine when the block of time is future, current, or past by changing the background color.
+
+## GitHub Links
+
+Live Page [Link](https://jeffcela.github.io/05-Third-Party-APIs/) <br>
+Project Repository [Link](https://github.com/jeffcela/05-Third-Party-APIs)
+
+## How its made!
+
+Make sure to clone or download the entire repository as this document uses **HTML, CSS, GIFs and JavaScript files**. If you add in any additional features, please comment where you added them to make it easier for other developers to follow, you will see that comments have been left for future upgrades or tinkering
+
+**What is included:**
+
+- index.html
+- gitignore (Used to prevent items from being added to the repo)
+- assets folder
+  - css folder
+    - style.css
+  - js folder
+    - script.js
+- README.md
+
+_Please make sure that you are adding any additional assets as needed_
+
+## This is what the Work Day Scheduler looks like!
+
+![](assets/images/scheduler.jpg)
+
+# Credit<br>
+
+_This homework assignment was completed in VS Code and Terminal by [Jeff Cela](https://www.linkedin.com/in/jeffcela/) on the day of 07/25/2021_<br>
+© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.<br>
+_HTML._ W3Schools Online Web Tutorials. (1999). [https://www.w3schools.com/](https://www.w3schools.com/).<br>
+Animated GIF Maker - Make GIFs from Images or Videos. (2012). https://gifmaker.me/.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// This is how the tasks get saved to the users local storage, it is done by each seperate hour.
     $("#hr9 .description").val(localStorage.getItem("hr9"));
     $("#hr10 .description").val(localStorage.getItem("hr10"));
     $("#hr11 .description").val(localStorage.getItem("hr11"));
@@ -8,6 +9,7 @@
     $("#hr16 .description").val(localStorage.getItem("hr16"));
     $("#hr17 .description").val(localStorage.getItem("hr17"));
 
+// this established the current date and time to allow the timeTracker function to know when to execute certain portions of the function
 $(document).ready(function () {
     $("#clock").text(moment().format("MMMM Do YYYY, h:mm a"));
     $(".saveBtn").on("click", function () {
@@ -16,5 +18,33 @@ $(document).ready(function () {
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
     })
+// This is the function that runs when the hour changes
+    function timeTracker() {
+        var currentHour = moment().hour();
+        $(".time-block").each(function () {
+            var taskHour = parseInt($(this).attr("id").split("hr")[1]);
+            console.log( taskHour, currentHour)
+// This adds the class past (grey) and removes future(green) and present(red) so that the user knows the time block is past due
+            if (taskHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+// This adds the class present (red) and removes past(grey) and future(green) so that the user knows the time block is due in the current hour         
+            else if (taskHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+// This adds the class future (green) and removes past(grey) and present(red) so that the user knows the time block is due in the future            
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+// this execute the color changing function
+    timeTracker();
 })
 
